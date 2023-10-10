8e03fa7fdaf1f4058f5ce9dab62e45da267259e9
vanessa-nkst <vanessa.nakasato2ufms.br>
2023-10-09 21:30:14 -0400

    first on

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Uchinanchu Taikai",
+    "description": "O festinaval okinawana ocorre a cada 5 anos. A última edição foi em 2021 e reuniu descentes de okinawados do mundo inteiro.",
+    "photoCover":"https://uchinanchutaikai2022.com.br/wp-content/uploads/2021/11/7o-Uchinanchu-taikai-postagens-blog.png",
+  },
+  {
+    "id":"2",
+    "title": "Castelo de Shuri",
+    "description": "O Castelo de Shuri é um marco do Reino de Ryukyu e é considerado Patrimônio Mundial pela UNESCO. ",
+    "photoCover":"https://s2.glbimg.com/bRMcojDqeATph-OlPUT4QsCpo_c=/e.glbimg.com/og/ed/f/original/2019/11/03/castelo_shuri.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Dialeto Okinawano ainda é mantido no Brasil",
+    "description": "O Brasil é destino de japoneses que desejam aprender mais sobre o dialeto e a cultura de Okinawa.",
+    "photoCover":"https://ichef.bbci.co.uk/news/800/cpsprodpb/600D/production/_99698542_img_5786.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Bon Odori - Uma homenagem aos antepassados",
+    "description": "O evento que ocorre em meados de agosto em Campo Grande/MS, é uma festa em homenagem aos antepassados e uma fomra de comemorar a colheita.",
+    "photoCover":"https://cdn.acritica.net/img/pc/920/600/dn_noticia/2016/08/bonodorijpg.JPG"
+  }
+]
